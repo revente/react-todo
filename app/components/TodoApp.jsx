3a600841ee49702df0ee161a16c92705ab9c1e92
revente <aurora_trd21@hotmail.com>
2016-10-30 14:16:40 +0000

    Todo App component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>TODO APP</p>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
